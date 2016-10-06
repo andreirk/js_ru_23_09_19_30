a5d970636efe3c470902cf48b5498231de0c231b
Andrey Sokolov <sokolik_85@list.ru>
2016-10-06 15:47:17 +0700

    a comment list component and button for it

--- src/Article.js
+++ src/Article.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react'
 
+import CommentList from './CommentList'
+
 export default class Article extends Component {
 
-    state = {
+  state = {
         isOpen: false,
         opened: {
             a: true
-        }
-    }
+        },
+        isClicked : false,
+}
 
     render() {
         const { article } = this.props
         const { isOpen } = this.state
+        const { isClicked } = this.state
 
         const body = isOpen ? <section>{article.text}</section> : null
 
+        console.log('is clicked is ' + isClicked);
+
+        const commentsBody = isClicked ? <CommentList comments = {article.comments} > {isOpen} </CommentList> : null
+
         return (
             <div>
-                <h3 onClick = {this.toggleOpen}>{article.title}</h3>
+                <h3 onClick = {this.toggleOpen} >{article.title}</h3>
                 {body}
+                <button onClick = {this.toggleCommentsOpen} > {isClicked ?  'hide comments' : 'Show comments'} </button >
+
+                {commentsBody}
+                
             </div>
         )
     }
@@ -26,26 +38,30 @@ export default class Article extends Component {
     toggleOpen = ev => {
         this.setState({
             isOpen: !this.state.isOpen
-        })
+        })    
 /*
-
         this.setState({
             opened: {...this.state.opened, a: false}
         })
 */
     }
+
+    toggleCommentsOpen = ev => {
+        console.log('iscliked is ' + this.state.isClicked )
+        this.setState({
+            isClicked: !this.state.isClicked
+        })
+    }
 }
-
-
 
 /*
 export default (props) => {
-    const { article } = props
+    const { comment } = props
 
     return (
         <div>
-            <h3>{article.title}</h3>
-            <section>{article.text}</section>
+            <h3>{comment.title}</h3>
+            <section>{comment.text}</section>
         </div>
     )
 }*/
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import ArticleList from './ArticleList'
-import { articles } from './fixtures'
+import { articles} from './fixtures'
+
 
 render(<ArticleList articles = {articles} />, document.getElementById('container'))
